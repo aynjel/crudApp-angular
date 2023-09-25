0043a80748d516061a5ad83c5f785ca31a5f7963
Angel Ni¤o C. Ortega <ortegacanillo76@gmail.com>
2023-09-25 10:57:28 +0800

    update api url

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,25 +7,27 @@ import { User } from '../interface/user';
 })
 export class UserService {
 
+  private api_url = 'http://localhost:8085/api/users';
+
   constructor(private _http: HttpClient) { }
 
   addUser(user: User){
-    return this._http.post('http://localhost:8085/api/users/add', user);
+    return this._http.post(`${this.api_url}/create`, user);
   }
 
   getUsers(){
-    return this._http.get('http://localhost:8085/api/users');
+    return this._http.get(`${this.api_url}`);
   }
 
   deleteUser(id: number){
-    return this._http.delete(`http://localhost:8085/api/users/delete/${id}`);
+    return this._http.delete(`${this.api_url}/${id}/delete`);
   }
 
   getUserById(id: number){
-    return this._http.get(`http://localhost:8085/api/users/${id}`);
+    return this._http.get(`${this.api_url}/${id}`);
   }
 
   updateUser(id: number, user: User){
-    return this._http.put(`http://localhost:8085/api/users/update/${id}`, user);
+    return this._http.put(`${this.api_url}/${id}/update`, user);
   }
 }
